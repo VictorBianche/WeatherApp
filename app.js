ca24f2b98a1dbdd0acbc42b2b26c2083e8633091
Victor Bianche <victorbianche@hotmail.com>
2022-02-04 13:41:55 -0300

    improvement and api fetch added

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 // patterns API
 const api_link = 'http://api.weatherapi.com/v1/current.json?key=';
 const api_key = 'b59319a383204b5483f13659221701&q=';
+const link = [];
 // Geo Localization setup
 let sucess = (pos) => crd = pos.coords;
 
 navigator.geolocation.getCurrentPosition(sucess);
 // Api link builder
-clique();
 
 function montaLink(){
     link_final = api_link + api_key + crd.latitude + ',' + crd.longitude;
-    console.log(link_final)};
-
-function clique(){
-    document.getElementById("btn").onclick = montaLink
+    return link_final
 };
 
+document.getElementById("btn").onclick = function Atribui(){
+    link.push(montaLink())
 
 
-
-
-
-
+    fetch(link[0])
+        .then(T => T.json())
+        .then(console.log)
+}
 
 
